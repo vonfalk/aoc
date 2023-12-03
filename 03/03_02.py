059bb4187cf3a011f513bc86c9460760d2d61a3c
Oscar Falk <oscar.falk2@gmail.com>
2023-12-03 22:06:46 +0100

    Add solution for 03_02

--- 03/03_02.py
+++ 03/03_02.py
@@ -8,17 +8,38 @@ maxLines = len(input)
 sum = 0
 
 
-def clamp(nr, lower, upper):
-    return max(lower, min(nr, upper))
+def isSurrounding(pos, span):
+    return (span[0] - 1 <= pos and span[0] + 1 >= pos) or (
+        span[1] - 1 <= pos and span[1] >= pos
+    )
 
 
-def countNumbers():
-    pass
+def getNearbyMatches(pos, line):
+    matches = []
+    for match in finditer(numberPattern, line):
+        if isSurrounding(pos, match.span()):
+            matches.append(int(match.group(0)))
+    return matches
+
+
+def calculateGearRatio(lineNr, pos):
+    matches = []
+    if lineNr > 0:
+        matches += getNearbyMatches(pos, input[lineNr - 1])
+    matches += getNearbyMatches(pos, input[lineNr])
+    if lineNr < maxLines - 1:
+        matches += getNearbyMatches(pos, input[lineNr + 1])
+    if len(matches) == 2:
+        return matches[0] * matches[1]
+    return 0
 
 
 for lineNr in range(0, maxLines):
-    line = input[lineNr]
-    lineLength = len(line)
-    symbolMatches = finditer(symbolPattern, line)
+    symbolMatches = finditer(symbolPattern, input[lineNr])
     for match in symbolMatches:
-        pass
+        sum += calculateGearRatio(lineNr, match.span()[0])
+
+print(sum)
+
+# INCORRECT ANSWERS:
+# 	467835 (too low)
