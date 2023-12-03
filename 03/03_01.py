0c4c6e7d365e10e761ea74f3f92f47a80018dc1a
Oscar Falk <oscar.falk2@gmail.com>
2023-12-03 16:05:39 +0100

    Add solution for 03_01

--- 03/03_01.py
+++ 03/03_01.py
@@ -1,18 +1,43 @@
-from re import finditer
+from re import finditer, search
 
 numberPattern = r"[0-9]+"
-charPattern = r"[^.0-9]+"
+symbolPattern = r"[^.\n]+"
 
 input = open("03_input.txt").readlines()
 maxLines = len(input)
+sum = 0
 
 
-def isPartNumber():
-    pass
+def clamp(nr, lower, upper):
+    return max(lower, min(nr, upper))
 
 
-for i in range(0, maxLines):
-    line = input[i]
+def surroundingText(lineNr, span, lineLength):
+    text = ""
+    if lineNr > 0:
+        text += input[lineNr - 1][
+            clamp(span[0] - 1, 0, lineLength) : clamp(span[1] + 1, 0, lineLength)
+        ]
+    if span[0] > 0:
+        text += input[lineNr][span[0] - 1]
+    if span[1] < lineLength - 1:
+        text += input[lineNr][span[1]]
+    if lineNr < maxLines - 1:
+        text += input[lineNr + 1][
+            clamp(span[0] - 1, 0, lineLength) : clamp(span[1] + 1, 0, lineLength)
+        ]
+    return text
+
+
+for lineNr in range(0, maxLines):
+    line = input[lineNr]
+    lineLength = len(line)
     numberMatches = finditer(numberPattern, line)
     for match in numberMatches:
-        print(match)
+        if search(symbolPattern, surroundingText(lineNr, match.span(), lineLength)):
+            sum += int(match.group(0))
+
+print(sum)
+
+# INCORRECT ANSWERS:
+#   554887 (too high)
