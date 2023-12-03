7e69eadf129efcb496406b8974f61a405c561d72
Oscar Falk <oscar.falk2@gmail.com>
2023-12-03 08:40:47 +0100

    Add input logic for 03_01

--- 03/03_01.py
+++ 03/03_01.py
@@ -1,1 +1,10 @@
+# from re import findall
+
+numberPattern = "[0-9]+"
+charPattern = "[^.0-9]"
+
 input = open("03_input.txt")
+maxLines = len(input.readline())
+
+for i in range(1, maxLines):
+    pass
