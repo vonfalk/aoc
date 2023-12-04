35b3814161d6d3a5b37d2afd5f227312478d6010
Oscar Falk <oscar.falk2@gmail.com>
2023-12-04 08:51:07 +0100

    Add solution for 04_01

---
+++ 04/04_01.py
@@ -0,0 +1,24 @@
+from re import findall
+
+numberPattern = r"[0-9]+"
+input = open("04_input.txt").readlines()
+
+
+def getScore(score):
+	if score == 0:
+		return 0
+	return pow(2, score - 1)
+
+
+score = 0
+for line in input:
+	numbers = line.split(": ")[1].split(" | ")
+	lhs = findall(numberPattern, numbers[0])
+	rhs = findall(numberPattern, numbers[1])
+	count = 0
+	for nr in lhs:
+		if nr in rhs:
+			count += 1
+	score += getScore(count)
+
+print(score)
